aa0154bd8e3cd6da5c75a3a433112a0c85585321
Haoyang Gao <gaohaoyang126@126.com>
2024-03-11 13:55:54 -0400

    feat: 🎸 architecture

---
+++ challenge009IsometricRoom/controls.ts
@@ -0,0 +1,8 @@
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { camera } from './camera'
+import { renderer } from './renderer'
+
+// Controls
+export const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+// controls.autoRotate = true
