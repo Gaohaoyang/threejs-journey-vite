f2b9439be811c88f7af197cd950bdcffd7a31a75
Haoyang Gao <gaohaoyang126@126.com>
2024-03-12 15:27:55 -0400

    feat: 🎸 Coordinate transformation

--- challenge009IsometricRoom/axesHelper.ts
+++ challenge009IsometricRoom/axesHelper.ts
@@ -1,11 +1,12 @@
 import { AxesHelper } from 'three'
 import { scene } from './scene'
+import { ny, floorXLength, floorZLength } from './objectFloorAndWalls'
 
 /**
  * AxesHelper
  */
-const axesHelper = new AxesHelper(20)
-axesHelper.position.set(0, 0, 0)
+const axesHelper = new AxesHelper(42)
+axesHelper.position.set(-floorXLength / 2, -ny, -floorZLength / 2)
 // axesHelper.visible = false
 
 scene.add(axesHelper)
--- challenge009IsometricRoom/objectFloorAndWalls.ts
+++ challenge009IsometricRoom/objectFloorAndWalls.ts
@@ -1,12 +1,21 @@
-import { Mesh, MeshStandardMaterial, Group } from 'three'
+import { Mesh, MeshStandardMaterial, Group, BoxHelper } from 'three'
 import { scene } from './scene'
 import { RoundedBoxGeometry } from 'three/addons/geometries/RoundedBoxGeometry.js'
 import { SUBTRACTION, Brush, Evaluator } from 'three-bvh-csg'
 
+export const floorXLength = 40.0
+export const floorZLength = 30.0
+const wallHeight = 26.8
+const wallThickness = 0.8
+const roundRadius = 0.2
+const roundSegments = 8
+export const ny = 18
+const wireframe = false
+
 const transparentMaterial = new MeshStandardMaterial({
   roughness: 1,
   metalness: 0,
-  wireframe: false,
+  wireframe,
   transparent: true,
   opacity: 0,
 })
@@ -14,14 +23,20 @@ const transparentMaterial = new MeshStandardMaterial({
 /**
  * Floor
  */
-const floorGeometry = new RoundedBoxGeometry(40.0, 0.8, 30.0, 8, 0.1)
+const floorGeometry = new RoundedBoxGeometry(
+  floorXLength,
+  wallThickness,
+  floorZLength,
+  roundSegments,
+  roundRadius,
+)
 const floorMaterial = new MeshStandardMaterial({
   roughness: 1,
   metalness: 0,
-  wireframe: false,
+  wireframe,
 })
 const floor = new Mesh(floorGeometry, floorMaterial)
-floor.position.set(0, -0.4, 0)
+floor.position.set(floorXLength / 2, -wallThickness / 2, floorZLength / 2)
 floor.receiveShadow = true
 floor.castShadow = true
 
@@ -31,58 +46,110 @@ floor.castShadow = true
 const wallMaterial = new MeshStandardMaterial({
   roughness: 1,
   metalness: 0,
-  wireframe: false,
+  wireframe,
 })
-const leftWallGeometry = new RoundedBoxGeometry(0.8, 26.8, 30.0, 8, 0.2)
-const lefWall = new Mesh(leftWallGeometry, wallMaterial)
-lefWall.position.set(-20.4, 12.6, 0)
-lefWall.receiveShadow = true
-lefWall.castShadow = true
+const wallNXGeometry = new RoundedBoxGeometry(
+  wallThickness,
+  wallHeight,
+  floorZLength,
+  roundSegments,
+  roundRadius,
+)
+const wallNX = new Mesh(wallNXGeometry, wallMaterial)
+wallNX.position.set(-wallThickness / 2, wallHeight / 2 - wallThickness, floorZLength / 2)
+wallNX.receiveShadow = true
+wallNX.castShadow = true
 
-const rightWallGeometry = new RoundedBoxGeometry(40.0 + 0.8, 26.8, 0.8, 8, 0.2)
-const rightWall = new Brush(rightWallGeometry, wallMaterial)
-rightWall.position.set(-0.4, 13.4 - 0.8, -15.0 - 0.4)
-rightWall.updateMatrixWorld()
+const wallNZGeometry = new RoundedBoxGeometry(
+  floorXLength + wallThickness,
+  wallHeight,
+  wallThickness,
+  roundSegments,
+  roundRadius,
+)
+const wallNZ = new Brush(wallNZGeometry, wallMaterial)
+wallNZ.position.set(
+  floorXLength / 2 - wallThickness / 2,
+  wallHeight / 2 - wallThickness,
+  -wallThickness / 2,
+)
+wallNZ.updateMatrixWorld()
 
-const windowOnRightWallGeometry = new RoundedBoxGeometry(40.0 + 0.8 - 10, 26.8 - 10, 0.8, 8, 0.2)
-const windowOnRightWall = new Brush(windowOnRightWallGeometry, wallMaterial)
-windowOnRightWall.position.set(-0.4, 13.4 - 0.8, -15.0 - 0.4)
-windowOnRightWall.updateMatrixWorld()
+const windowOnWallNZGeometry = new RoundedBoxGeometry(
+  20,
+  18,
+  wallThickness,
+  roundSegments,
+  roundRadius,
+)
+const windowOnWallNZ = new Brush(windowOnWallNZGeometry, wallMaterial)
+windowOnWallNZ.position.set(20, 12, -wallThickness / 2)
+windowOnWallNZ.updateMatrixWorld()
 
 const evaluator = new Evaluator()
-const windowOnRightWallCSG = evaluator.evaluate(rightWall, windowOnRightWall, SUBTRACTION)
-windowOnRightWallCSG.receiveShadow = true
-windowOnRightWallCSG.castShadow = true
+const windowOnWallNZCSG = evaluator.evaluate(wallNZ, windowOnWallNZ, SUBTRACTION)
+windowOnWallNZCSG.receiveShadow = true
+windowOnWallNZCSG.castShadow = true
 
-const frontWallGeometry = new RoundedBoxGeometry(40.0 + 0.8, 26.8, 0.8, 8, 0.2)
-const frontWall = new Mesh(frontWallGeometry, transparentMaterial)
-frontWall.position.set(-0.4, 13.4 - 0.8, 15.0 + 0.4)
-frontWall.receiveShadow = true
-frontWall.castShadow = true
+const wallPZGeometry = new RoundedBoxGeometry(
+  floorXLength + wallThickness + wallThickness,
+  wallHeight,
+  wallThickness,
+  roundSegments,
+  roundRadius,
+)
+const wallPZ = new Mesh(wallPZGeometry, transparentMaterial)
+wallPZ.position.set(
+  floorXLength / 2,
+  wallHeight / 2 - wallThickness,
+  floorZLength + wallThickness / 2,
+)
+wallPZ.receiveShadow = true
+wallPZ.castShadow = true
 
-const backWallGeometry = new RoundedBoxGeometry(0.8, 26.8, 30.0 + 0.8, 8, 0.2)
-const backWall = new Mesh(backWallGeometry, transparentMaterial)
-backWall.position.set(20.4, 12.6, -0.4)
-backWall.receiveShadow = true
-backWall.castShadow = true
+const wallPXGeometry = new RoundedBoxGeometry(
+  wallThickness,
+  wallHeight,
+  floorZLength + wallThickness,
+  roundSegments,
+  roundRadius,
+)
+const wallPX = new Mesh(wallPXGeometry, transparentMaterial)
+wallPX.position.set(
+  floorXLength + wallThickness / 2,
+  wallHeight / 2 - wallThickness,
+  floorZLength / 2 - wallThickness / 2,
+)
+wallPX.receiveShadow = true
+wallPX.castShadow = true
 
 /**
  * Roof
  */
-const roofGeometry = new RoundedBoxGeometry(40.0 + 0.8, 0.8, 30.0 + 0.8, 8, 0.2)
+const roofGeometry = new RoundedBoxGeometry(
+  floorXLength + wallThickness + wallThickness,
+  wallThickness,
+  floorZLength + wallThickness + wallThickness,
+  roundSegments,
+  roundRadius,
+)
 
 const roof = new Mesh(roofGeometry, transparentMaterial)
-roof.position.set(-0.4, 26.8 - 0.4, -0.4)
+roof.position.set(floorXLength / 2, wallHeight - wallThickness / 2, floorZLength / 2)
 roof.receiveShadow = true
 roof.castShadow = true
 
 // groupConstruction
 const groupConstruction = new Group()
+groupConstruction.position.set(-floorXLength / 2, -ny, -floorZLength / 2)
 
 groupConstruction.add(floor)
-groupConstruction.add(lefWall)
-groupConstruction.add(windowOnRightWallCSG)
-groupConstruction.add(frontWall)
-groupConstruction.add(backWall)
+groupConstruction.add(wallNX)
+groupConstruction.add(windowOnWallNZCSG)
+groupConstruction.add(wallPZ)
+groupConstruction.add(wallPX)
 groupConstruction.add(roof)
 scene.add(groupConstruction)
+
+export const groupConstructionBoxHelper = new BoxHelper(groupConstruction)
+scene.add(groupConstructionBoxHelper)
