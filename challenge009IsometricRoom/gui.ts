525c0fb7b84a9eb6f5c6c567a781609b6a983507
Haoyang Gao <gaohaoyang126@126.com>
2024-03-12 23:21:24 -0400

    feat: 🎸 light

--- challenge009IsometricRoom/gui.ts
+++ challenge009IsometricRoom/gui.ts
@@ -1,7 +1,12 @@
 import GUI from 'lil-gui'
+import { directionalLight, pointLight } from './lights'
 
 // Debug GUI
 const gui = new GUI()
 const debugObject = {}
 
 export { gui, debugObject }
+
+gui.add(directionalLight, 'visible').name('directionalLight')
+
+gui.add(pointLight, 'visible').name('pointLight')
