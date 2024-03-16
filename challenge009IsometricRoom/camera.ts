a79e2357c14ddc0fe5c5eeb24262105f61d26e55
Haoyang Gao <gaohaoyang126@126.com>
2024-03-15 23:11:10 -0400

    feat: 🎸 sizes.width < 768

--- challenge009IsometricRoom/camera.ts
+++ challenge009IsometricRoom/camera.ts
@@ -6,4 +6,10 @@ import { sizes } from './utils'
 // Base camera
 // const aspectRatio = sizes.width / sizes.height
 export const camera = new PerspectiveCamera(20, sizes.width / sizes.height, 0.1, 100000)
-camera.position.set(120, 100, 120)
+
+// if on mobile
+if (sizes.width < 768) {
+  camera.position.set(200, 175, 200)
+} else {
+  camera.position.set(120, 100, 120)
+}
