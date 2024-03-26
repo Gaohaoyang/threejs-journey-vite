258c488947d17dc62672c030361a80b7b765c47c
Haoyang Gao <gaohaoyang126@126.com>
2024-03-26 00:21:07 -0400

    feat: 🎸 screen

--- challenge009IsometricRoom/objectScreen.ts
+++ challenge009IsometricRoom/objectScreen.ts
@@ -1,30 +1,70 @@
-import { MeshStandardMaterial, DoubleSide, Mesh, BoxGeometry, Group } from 'three'
-import { wireframe, floorAndWalls } from './objectConstant'
+import { MeshStandardMaterial, Mesh, BoxGeometry, Group } from 'three'
 import { scene } from './scene'
-import * as CANNON from 'cannon-es'
-import { world } from './physicsWorld'
 
-const screenWidth = 20
-const screenHeight = 18
+const screenWidth = 16 * 1.8
+const screenHeight = 10 * 1.8
 const screenThickness = 0.02
-const screenItemNum = 100
+const screenItemNum = 80
 const screenItemHeight = screenHeight / screenItemNum
 
 const screenItemGeometry = new BoxGeometry(screenWidth, screenItemHeight, screenThickness)
 const screenItemMaterial = new MeshStandardMaterial({
+  metalness: 0.01,
+  roughness: 0.8,
   color: 0xffffff,
-  side: DoubleSide,
   wireframe: false,
 })
 const screenItem = new Mesh(screenItemGeometry, screenItemMaterial)
 
 const screenGroup = new Group()
+const screenItems: Mesh[] = []
 
 for (let i = 0; i < screenItemNum; i++) {
   const item = screenItem.clone()
-  item.position.set(-16, screenHeight / 2 - screenItemHeight * i - 3, 0)
+  item.position.set(-16, screenHeight / 2 - screenItemHeight * i - 2, 0)
   item.rotateY(Math.PI / 2)
-  screenGroup.add(item)
+  screenItems.push(item)
+  item.castShadow = true
+  item.receiveShadow = true
+}
+screenGroup.add(...screenItems)
+
+const moveSpeed = 0.1
+let isLifting = false
+let isDropping = false
+let dropStatus = true
+export const screenLift = () => {
+  isLifting = true
+  isDropping = false
+}
+export const screenDrop = () => {
+  isDropping = true
+  isLifting = false
+}
+export const screenAnimation = (delta: number) => {
+  if (isLifting) {
+    if (screenItems[screenItems.length - 1].position.y < screenHeight / 2 - 2) {
+      screenItems.forEach((item, index) => {
+        item.position.y += delta * moveSpeed * index
+      })
+    }
+  } else if (isDropping) {
+    if (screenItems[screenItems.length - 1].position.y > -screenHeight / 2 - 2 + 0.5) {
+      screenItems.forEach((item, index) => {
+        item.position.y -= delta * moveSpeed * index
+      })
+    }
+  }
+}
+
+export const toggleDropAndLift = () => {
+  if (dropStatus) {
+    screenLift()
+    dropStatus = false
+  } else {
+    screenDrop()
+    dropStatus = true
+  }
 }
 
 scene.add(screenGroup)
