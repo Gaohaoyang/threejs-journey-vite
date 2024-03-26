1c53b639c3779890eab47cf9bfc28058379aaf43
Haoyang Gao <gaohaoyang126@126.com>
2024-03-26 01:01:59 -0400

    feat: 🎸 rmmm

--- challenge009IsometricRoom/objectCurtain.ts
+++ challenge009IsometricRoom/objectCurtain.ts
@@ -1,94 +1,94 @@
-import { PlaneGeometry, MeshStandardMaterial, DoubleSide, Mesh } from 'three'
-import { wireframe, floorAndWalls } from './objectConstant'
-import { scene } from './scene'
-import * as CANNON from 'cannon-es'
-import { world } from './physicsWorld'
-
-const { floorXLength, floorZLength, ny } = floorAndWalls
-const Nx = 16
-const Ny = 16
-const mass = 1
-// const clothSize = 10
-const clothSize = floorXLength / 2
-const dist = clothSize / Nx
+// import { PlaneGeometry, MeshStandardMaterial, DoubleSide, Mesh } from 'three'
+// import { wireframe, floorAndWalls } from './objectConstant'
+// import { scene } from './scene'
+// import * as CANNON from 'cannon-es'
+// // import { world } from './physicsWorld'
 
 // const { floorXLength, floorZLength, ny } = floorAndWalls
-// const curtainWidth = floorXLength / 2
-const curtainHeight = floorAndWalls.wallHeight - 2.6
+// const Nx = 16
+// const Ny = 16
+// const mass = 1
+// // const clothSize = 10
+// const clothSize = floorXLength / 2
+// const dist = clothSize / Nx
 
-const curtainGeometry = new PlaneGeometry(clothSize, clothSize, Nx, Ny)
-const curtainMaterial = new MeshStandardMaterial({
-  wireframe,
-  color: 0xddeeaa,
-  // transparent: true,
-  // opacity: 0.5,
-  side: DoubleSide,
-})
+// // const { floorXLength, floorZLength, ny } = floorAndWalls
+// // const curtainWidth = floorXLength / 2
+// const curtainHeight = floorAndWalls.wallHeight - 2.6
 
-const curtain = new Mesh(curtainGeometry, curtainMaterial)
-curtain.position.set(9, -ny + curtainHeight / 2 + 2, -floorZLength / 2 + 0.5)
+// const curtainGeometry = new PlaneGeometry(clothSize, clothSize, Nx, Ny)
+// const curtainMaterial = new MeshStandardMaterial({
+//   wireframe,
+//   color: 0xddeeaa,
+//   // transparent: true,
+//   // opacity: 0.5,
+//   side: DoubleSide,
+// })
 
-curtain.castShadow = true
-curtain.receiveShadow = true
+// const curtain = new Mesh(curtainGeometry, curtainMaterial)
+// curtain.position.set(9, -ny + curtainHeight / 2 + 2, -floorZLength / 2 + 0.5)
 
-scene.add(curtain)
+// curtain.castShadow = true
+// curtain.receiveShadow = true
 
-/**
- * physics
- */
-const shape = new CANNON.Particle()
+// scene.add(curtain)
 
-const particles: CANNON.Body[][] = []
-for (let i = 0; i < Nx + 1; i++) {
-  particles.push([])
-  for (let j = 0; j < Ny + 1; j++) {
-    console.log(j === Ny && i % 4 === 0)
-    const particle = new CANNON.Body({
-      mass: j === Ny && i % 4 === 0 ? 0 : mass,
-      shape,
-      position: new CANNON.Vec3((i - Nx * 0.5) * dist, (j - Ny * 0.5) * dist, 0),
-      sleepSpeedLimit: 0.3,
-      // velocity: new CANNON.Vec3(0, 0, -20),
-    })
-    particles[i].push(particle)
-    world.addBody(particle)
-  }
-}
+// /**
+//  * physics
+//  */
+// const shape = new CANNON.Particle()
 
-console.log(particles)
+// const particles: CANNON.Body[][] = []
+// for (let i = 0; i < Nx + 1; i++) {
+//   particles.push([])
+//   for (let j = 0; j < Ny + 1; j++) {
+//     console.log(j === Ny && i % 4 === 0)
+//     const particle = new CANNON.Body({
+//       mass: j === Ny && i % 4 === 0 ? 0 : mass,
+//       shape,
+//       position: new CANNON.Vec3((i - Nx * 0.5) * dist, (j - Ny * 0.5) * dist, 0),
+//       sleepSpeedLimit: 0.3,
+//       // velocity: new CANNON.Vec3(0, 0, -20),
+//     })
+//     particles[i].push(particle)
+//     world.addBody(particle)
+//   }
+// }
 
-const connect = (i1: number, j1: number, i2: number, j2: number) => {
-  world.addConstraint(new CANNON.DistanceConstraint(particles[i1][j1], particles[i2][j2], dist))
-}
+// console.log(particles)
 
-for (let i = 0; i < Nx + 1; i++) {
-  for (let j = 0; j < Ny + 1; j++) {
-    if (i < Nx) connect(i, j, i + 1, j)
-    if (j < Ny) connect(i, j, i, j + 1)
-  }
-}
+// const connect = (i1: number, j1: number, i2: number, j2: number) => {
+//   world.addConstraint(new CANNON.DistanceConstraint(particles[i1][j1], particles[i2][j2], dist))
+// }
 
-export const updateParticles = () => {
-  for (let i = 0; i < Nx + 1; i++) {
-    for (let j = 0; j < Ny + 1; j++) {
-      const index = j * (Nx + 1) + i
+// for (let i = 0; i < Nx + 1; i++) {
+//   for (let j = 0; j < Ny + 1; j++) {
+//     if (i < Nx) connect(i, j, i + 1, j)
+//     if (j < Ny) connect(i, j, i, j + 1)
+//   }
+// }
 
-      const positionAttribute = curtainGeometry.attributes.position
+// export const updateParticles = () => {
+//   for (let i = 0; i < Nx + 1; i++) {
+//     for (let j = 0; j < Ny + 1; j++) {
+//       const index = j * (Nx + 1) + i
 
-      const position = particles[i][Ny - j].position
+//       const positionAttribute = curtainGeometry.attributes.position
 
-      positionAttribute.setXYZ(index, position.x, position.y, position.z)
+//       const position = particles[i][Ny - j].position
 
-      positionAttribute.needsUpdate = true
-    }
-  }
-}
+//       positionAttribute.setXYZ(index, position.x, position.y, position.z)
 
-export const openCurtain = () => {
-  for (let i = 0; i < Nx + 1; i++) {
-    for (let j = 0; j < Ny + 1; j++) {
-      const particle = particles[i][j]
-      particle.velocity.set(0, 0, -20)
-    }
-  }
-}
+//       positionAttribute.needsUpdate = true
+//     }
+//   }
+// }
+
+// export const openCurtain = () => {
+//   for (let i = 0; i < Nx + 1; i++) {
+//     for (let j = 0; j < Ny + 1; j++) {
+//       const particle = particles[i][j]
+//       particle.velocity.set(0, 0, -20)
+//     }
+//   }
+// }
